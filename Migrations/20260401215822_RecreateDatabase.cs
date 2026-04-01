ac0c1f4e9b81ae7af6fa6de46ed43c3a933ef425
Xsessha <olkhovskayxseniia@gmail.com>
2026-04-02 01:00:17 +0300

    fix

---
+++ Migrations/20260401215822_RecreateDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HORIZON1.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- obj/Debug/net8.0/HORIZON1.AssemblyInfo.cs
+++ obj/Debug/net8.0/HORIZON1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HORIZON1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73cea75c523c0269762e4410b2e6b3e69c0067b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a36bc318e20626b8209c23380d8cd704eb038cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HORIZON1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HORIZON1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
